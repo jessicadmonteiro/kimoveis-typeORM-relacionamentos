c4e875e7e8bb27990c60d4922dfed20724b19f34
jessica <jessica_02_06@outlook.com>
2023-03-07 13:33:20 -0300

    fix: listagem realEstate

--- src/interfaces/realEstate.interfaces.ts
+++ src/interfaces/realEstate.interfaces.ts
@@ -1,13 +1,10 @@
 import { z } from "zod"
 import { createRealStateSchema, arrayRealEstateSchema } from "../schemas/realEstate.schemas"
 
-
-
 type iRealEstate = z.infer<typeof createRealStateSchema>
 type iRealEstateArray = z.infer<typeof arrayRealEstateSchema>
 
-
 export {
     iRealEstate,
-    iRealEstateArray
+    iRealEstateArray,
 }
--- src/services/realEstate/readRealEstate.services.ts
+++ src/services/realEstate/readRealEstate.services.ts
@@ -2,19 +2,19 @@ import { Repository } from "typeorm"
 import { AppDataSource } from "../../data-source"
 import { RealEstate } from "../../entities"
 import { iRealEstateArray } from "../../interfaces/realEstate.interfaces"
-import { arrayRealEstateSchema } from "../../schemas/realEstate.schemas"
 
 const readRealEstateService = async (): Promise<iRealEstateArray> => {
     
     const realEstateRepository: Repository<RealEstate> = AppDataSource.getRepository(RealEstate)
 
-    const findRealEstate: Array<RealEstate> = await realEstateRepository.find()
-
-    const realEstate: iRealEstateArray = arrayRealEstateSchema.parse(findRealEstate)
-
+    const realEstate: Array<RealEstate> = await realEstateRepository.find({
+        relations: {
+            address: true
+        }
+    })
+   
     return realEstate
 }
-
 
 export {
     readRealEstateService
